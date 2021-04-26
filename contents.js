265c53c6e629af5a2069496bed206aca896220bd
Francesco2499 <81304164+Francesco2499@users.noreply.github.com>
2021-04-26 10:17:36 +0200

    Update contents.js

--- contents.js
+++ contents.js
@@ -1,15 +1,15 @@
 const content= [
 {
-    immagine:'img_mhw3/img3.png',
+    immagine:'img3.png',
 titolo:'Struttura',
 descrizione:'Il nostro ampio parcheggio permette di raggiungere la struttura nel modo più semplice e comodo possibile. Scopri dove poterci trovare.'},
 {
-    immagine:'img_mhw3/img4.jpg',
+    immagine:'img4.jpg',
 titolo:'Reparti',
 descrizione:'I nostri reparti sono adatti alle necessità operative della realtà attuale. Sono definite regole di accesso per garantire la sicurezza a tutti i pazienti.'
 },
 {
-    immagine:'img_mhw3/img5.jpg',
+    immagine:'img5servizi.jpg',
 titolo:'Servizi',
 descrizione:'Cliccando qui sotto troverai un elenco dei servizi di maggiore importanza in questo periodo.'
 }
