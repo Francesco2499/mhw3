0705a64f265cb89e476ebda135393d0e35418f9a
Francesco2499 <81304164+Francesco2499@users.noreply.github.com>
2021-04-26 10:21:49 +0200

    Update contents.js

--- contents.js
+++ contents.js
@@ -1,10 +1,10 @@
 const content= [
 {
-    immagine:'img3.png',
+    immagine:'Ospedale.png',
 titolo:'Struttura',
 descrizione:'Il nostro ampio parcheggio permette di raggiungere la struttura nel modo più semplice e comodo possibile. Scopri dove poterci trovare.'},
 {
-    immagine:'img4.jpg',
+    immagine:'Reparti.jpg',
 titolo:'Reparti',
 descrizione:'I nostri reparti sono adatti alle necessità operative della realtà attuale. Sono definite regole di accesso per garantire la sicurezza a tutti i pazienti.'
 },
